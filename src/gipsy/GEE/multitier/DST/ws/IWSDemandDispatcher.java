9afc4ba1606bcdb267ab2c4aa068bddb52c742be
root <sun.prashanth99@gmail.com>
2016-04-22 21:37:44 -0400

    first commit

---
+++ src/gipsy/GEE/multitier/DST/ws/IWSDemandDispatcher.java
@@ -0,0 +1,19 @@
+package gipsy.GEE.multitier.DST.ws;
+
+import gipsy.GEE.IDP.DemandDispatcher.IDemandDispatcher;
+
+import java.rmi.Remote;
+
+/**
+ * An interface for WS Demand Dispatcher that extends the API from IDemandDispatcher interface 
+ *   and remote interface
+ * @author 
+ * @since 1.0.0
+ * @version $Id: IRMIDemandDispatcher.java,v :
+ */
+
+public interface IWSDemandDispatcher extends Remote, IDemandDispatcher{
+
+	
+	
+}
